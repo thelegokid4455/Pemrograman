762d182c9298ef97b8e6101f07c01aeca6ae60fa
FRI <farhanri.tp19@mail.mmtc.ac.id>
2020-09-18 16:47:00 +0800

    +added Latihan1-3
    18-9-20

---
+++ Unity - Pemrograman/Assets/Scripts/Pemrograman/Latihan1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Saya Farhan Ramadhani Irsyad adalah Game Developer");
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log("Waktu untuk FixedUpdate: " + Time.deltaTime);
+    }
+
+    void Update()
+    {
+        Debug.Log("Waktu untuk update: " + Time.deltaTime);
+    }
+
+    void LateUpdate()
+    {
+        Debug.Log("Waktu untuk LateUpdate: " + Time.deltaTime);
+    }
+}
