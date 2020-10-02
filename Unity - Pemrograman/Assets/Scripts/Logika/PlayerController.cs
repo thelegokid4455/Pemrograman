da35a36de9e26a499ac5e09c46df9ce7d74579c4
FRI <farhanri.tp19@mail.mmtc.ac.id>
2020-10-02 15:33:47 +0800

    +Cleaned folders per subject
    adjusted the folders per subject.
    -logika game
    -pemrograman game

--- Unity - Pemrograman/Assets/Scripts/Logika/PlayerController.cs
+++ Unity - Pemrograman/Assets/Scripts/Logika/PlayerController.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     public float speed;
+    public float jumpSpeed;
+    public Text counter;
+    public int coins;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +20,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        counter.text = "Coins: " + coins;
         
     }
 
@@ -25,6 +30,11 @@ public class PlayerController : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
 
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(new Vector3(0, jumpSpeed, 0));
+            }
         rb.AddForce(movement * speed);
     }
 }
